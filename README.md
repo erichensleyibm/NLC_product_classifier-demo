55019a678ca49ecd23d1f6e19d0a0fc8eab77698
erichensleyibm <eric.hensley@ibm.com>
2018-04-13 15:20:07 -0400

    initial_commit

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
-# NLU_product_classifier-demo
+> **DISCLAIMER**: This application is used for demonstrative and illustrative purposes only and does not constitute an offering that has gone through regulatory review. It is not intended to serve as a medical application. There is no representation as to the accuracy of the output of this application and it is presented without warranty.
+
+# Classify medical diagnosis with ICD-10 code
+
+This application was built to demonstrate IBM's Watson Natural Language Classifier (NLC). The data set we will be using, [ICD-10-GT-AA.csv](data/ICD-10-GT-AA.csv), contains a subset of [ICD-10](https://en.wikipedia.org/wiki/ICD-10) entries. ICD-10 is the 10th revision of the International Statistical Classification of Diseases and Related Health Problems. In short, it is a medical classification list by the World Health Organization (WHO) that contains codes for: diseases, signs and symptoms, abnormal findings, complaints, social circumstances, and external causes of injury or diseases. Hospitals and insurance companies alike could save time and money by levearging Watson to properly tag the most accurate ICD-10 codes.
+
+This application is a Python web application based on the [Flask microframework](http://flask.pocoo.org/), and based on earlier work done by [Ryan Anderson](https://github.com/rustyoldrake/IBM_Watson_NLC_ICD10_Health_Codes). It uses the [Watson Python SDK](https://github.com/watson-developer-cloud/python-sdk) to create the classifier, list classifiers, and classify the input text. We also make use of the freely available [ICD-10 API](http://icd10api.com/) which, given an ICD-10 code, returns a name and description.
+
+## Architecture
+
+![](images/architecture.png)
+
+## Setup the classifier
+
+Here we create the classifier with our ICD-10 dataset.
+
+1. Download the [ICD-10 dataset](https://raw.githubusercontent.com/stevemart/nlc-icd10-demo/master/data/ICD-10-GT-AA.csv) by right clicking the link and seletcting _Save As_.
+1. Create an [NLC service in IBM Cloud](https://console.bluemix.net/catalog/services/natural-language-classifier), make a note of the service name used in the catalog, we'll need this later.
+1. Create service credentials by using the menu on the left and selecting the default options.
+1. Upload the data using the command below. Be sure to substitute the username and password. This will take around 3 hours.
+
+```bash
+curl -i --user "$username":"$password" -F training_data=@ICD-10-GT-AA.csv -F training_metadata="{\"language\":\"en\",\"name\":\"ICD-10Classifier\"}" "https://gateway.watsonplatform.net/natural-language-classifier/api/v1/classifiers"
+````
+
+## Running the application
+
+This application can be run locally or hosted on IBM Cloud, follow the steps below depending on your deployment choice
+
+### Run locally
+
+1. Clone this project: `git clone git@github.com:IBM/nlc-icd10-demo.git`
+1. `cd` into this project's root directory
+1. (Optionally) create a virtual environment: `virtualenv my-nlc-demo`
+    1. Activate the virtual environment: `./my-nlc-demo/bin/activate`
+1. Run `pip install -r requirements.txt` to install the app's dependencies
+1. Update the [welcome.py](welcome.py) with your NLC credentials
+1. Run `python welcome.py`
+1. Access the running app in a browser at `http://localhost:5000`
+
+### Run on IBM Cloud
+
+1. Clone this project: `git clone git@github.com:IBM/nlc-icd10-demo.git`
+1. `cd` into this project's root directory
+1. Update [`manifest.yml`](manifest.yml) with the NLC service name (`your_nlc_service_name`), a unique application name (`your_app_name`) and unique host value (`your_app_host`)
+
+    ```
+    applications:
+      - path: .
+      memory: 256M
+      instances: 1
+      domain: mybluemix.net
+      name: your_app_name
+      host: your_app_host
+      disk_quota: 1024M
+      services:
+      - your_nlc_service_name
+      buildpack: python_buildpack
+    ```
+
+1. Run `bluemix app push` from the root directory
+1. Access the running app by going to: `https://<host-value>.mybluemix.net/`
+
+> If you've never run the `bluemix` command before there is some configuration required, refer to the official [IBM Cloud CLI](https://console.bluemix.net/docs/cli/reference/bluemix_cli/get_started.html) docs to get this set up.
+
+# Links
+* [Watson NLC API](https://www.ibm.com/watson/developercloud/natural-language-classifier/api/v1/)
+* [Watson Python SDK](https://github.com/watson-developer-cloud/python-sdk)
+* [IBM Cloud CLI](https://console.bluemix.net/docs/cli/reference/bluemix_cli/get_started.html)
+* [Watson Natural Language Classifier](https://www.ibm.com/watson/services/natural-language-classifier/)
+* [Ryan Anderson's Original Work](https://github.com/rustyoldrake/IBM_Watson_NLC_ICD10_Health_Codes)
+* [ICD-10 API](http://icd10api.com)
+* [ICD-10 on Wikipedia](https://en.wikipedia.org/wiki/ICD-10)
+* [Intro to NLC Tutorial](https://www.youtube.com/watch?v=SUj826ybCdU)
+
+# License
+
+[Apache 2.0](LICENSE)
---
+++ welcome.py
@@ -0,0 +1,153 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import os
+import requests
+
+import _config
+from flask import Flask, jsonify, render_template, request
+from watson_developer_cloud import NaturalLanguageClassifierV1
+from flask_table import Table, Col
+
+from lxml import html
+
+app = Flask(__name__)
+
+# The data set we want to use
+DATA_SET = 'data/product_description_training.csv'
+
+VCAP_SERVICES = os.getenv("VCAP_SERVICES")
+if VCAP_SERVICES is not None:
+    # These will be automatically set if deployed to IBM Cloud
+    SERVICES = json.loads(VCAP_SERVICES)
+    NLC_USERNAME = SERVICES['natural_language_classifier'][0]['credentials']['username']
+    NLC_PASSWORD = SERVICES['natural_language_classifier'][0]['credentials']['username']
+else:
+    try:
+    # Set these here for local development
+        NLC_USERNAME = _config.NLC_USERNAME
+        NLC_PASSWORD = _config.NLC_PASSWORD
+    except:
+        NLC_USERNAME = ""
+        NLC_PASSWORD = ""        
+    
+CLASSIFIER = None
+
+@app.route('/')
+def Welcome():
+    global CLASSIFIER
+    
+    try:
+        NLC_SERVICE = NaturalLanguageClassifierV1(
+        username=NLC_USERNAME,
+        password=NLC_PASSWORD      
+        )
+        global NLC_SERVICE
+    except:
+        NLC_SERVICE = False
+    
+    if NLC_SERVICE:
+        # create classifier if it doesn't exist, format the json
+        CLASSIFIER = _create_classifier()
+        classifier_info = json.dumps(CLASSIFIER, indent=4)
+        # update the UI, but only the classifier info box
+        return render_template('index.html', classifier_info=classifier_info, icd_code="", icd_output="", classifier_output="")
+    else:
+        return render_template('index.html', classifier_info="Please add a _config.py file with your NLC credentials if running locally. "  , icd_code="", icd_output="", classifier_output="")
+        
+
+@app.route('/classify_text', methods=['GET', 'POST'])
+def classify_text():
+    # get the text from the UI
+    input_text = request.form['classifierinput_text']
+    # get info about the classifier
+    classifier_info = json.dumps(CLASSIFIER, indent=4)
+
+    if input_text != '':
+        #send the text to the classifier, get back an ICD code
+        classifier_output = NLC_SERVICE.classify(CLASSIFIER['classifier_id'], input_text)
+        #format results
+        all_results = ResultsTable(classifier_output['classes'])
+#        classifier_output = json.dumps(classifier_output['top_class'], indent=4)
+        
+        #classifier_output = input_text
+        #classifier_output = json.dumps(classifier_output, indent=4)
+    
+        # fill in the text boxes
+        return render_template('index.html', classifier_info=classifier_info, classifier_input = input_text, all_results = all_results)
+    else:
+        return render_template('index.html', classifier_info=classifier_info, classifier_input = input_text, all_results = 'No description provided.')
+        
+@app.route('/classify_url', methods=['GET', 'POST'])
+def classify_url():
+    
+    # get info about the classifier
+    classifier_info = json.dumps(CLASSIFIER, indent=4)
+
+    # get the text from the UI    
+    input_url = request.form['classifierinput_url']
+    input_text = _get_Kohls_url_info(input_url)
+    
+    if input_text:
+        # send the text to the classifier, get back an ICD code
+        classifier_output = NLC_SERVICE.classify(CLASSIFIER['classifier_id'], input_text)
+        # format results
+        all_results = ResultsTable(classifier_output['classes'])
+#        classifier_output = json.dumps(classifier_output['top_class'], indent=4)
+        
+#        classifier_output = input_text
+    #    classifier_output = json.dumps(classifier_output, indent=4)
+    
+        # fill in the text boxes
+        return render_template('index.html', classifier_info=classifier_info, classifier_input = input_text, all_results = all_results)
+    else:
+        return render_template('index.html', classifier_info=classifier_info, classifier_input = input_text, all_results = 'Invalid Url')
+  
+class ResultsTable(Table):
+    table_id = 'classes'
+    class_name = Col('Class')
+    confidence = Col('Confidence')      
+
+def _create_classifier():
+    # fetch all classifiers associated with the NLC instance
+    result = NLC_SERVICE.list_classifiers()
+    # for the purposes of this demo, we handle only one classifier
+    # return the first one found
+    if len(result['classifiers']) > 0:
+        return result['classifiers'][0]
+    else:
+        # if none found, create a new classifier, change this value
+        with open(DATA_SET, 'rb') as training_data:
+            metadata = '{"name": "ICD_classifier", "language": "en"}'
+            classifier = NLC_SERVICE.create_classifier(
+                metadata=metadata,
+                training_data=training_data
+            )
+        return classifier
+
+def _get_Kohls_url_info(url):
+    if url[8:34] == 'www.kohls.com/product/prd-':
+        prd_id = url.split('/prd-')[1].split('/')[0]
+        raw_desc = []
+        while raw_desc == []:
+            pageContent=requests.get(url)
+            tree = html.fromstring(pageContent.content)
+            raw_desc = tree.xpath('//*[@id="%s_productDetails"]/div/descendant::*/text()' % (prd_id))
+            desc = ' '.join([i for i in raw_desc if i not in ['PRODUCT FEATURES', '\r']])
+        return desc
+    else:
+        return False
+
+port = os.getenv('PORT', '5000')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(port))
